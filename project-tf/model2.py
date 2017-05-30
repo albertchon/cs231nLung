50cc68c7c95ca79299742e834e5e495d3e434c42
achon22 <albertchon1@gmail.com>
2017-05-29 23:30:28 -0700

    model2.py

---
+++ project-tf/model2.py
@@ -0,0 +1,258 @@
+import time
+import logging
+
+import math
+import numpy as np
+from six.moves import xrange  # pylint: disable=redefined-builtin
+import tensorflow as tf
+from tensorflow.python.ops import variable_scope as vs
+import os
+import random
+
+
+
+logging.basicConfig(level=logging.INFO)
+
+class LungSystem(object):
+    def __init__(self, FLAGS):
+        self.FLAGS = FLAGS
+
+        # ==== set up placeholder tokens ========
+        # self.inputs_placeholder = tf.placeholder(tf.float32, shape=(None, self.FLAGS.output_size, self.FLAGS.embedding_size), name="x")
+        # self.paragraph = tf.placeholder(shape=[None, self.FLAGS.output_size])
+
+        self.images = tf.placeholder(tf.float32, shape=[None, FLAGS.num_slices, FLAGS.image_height, FLAGS.image_width])
+        self.labels = tf.placeholder(tf.int32, shape=[None])
+        self.is_training = tf.placeholder(tf.bool)
+        # ==== assemble pieces ====
+        with tf.variable_scope("lung_sys"):
+            if FLAGS.model == 'linear':
+                self.setup_linear_system()
+            else:
+                self.setup_cnn_system()
+            print('setting up loss')
+            self.setup_loss()
+
+            # ==== set up training/updating procedure ====
+        # global_step = tf.Variable(0, trainable=False)
+        # learning_rate = tf.train.polynomial_decay(self.FLAGS.learning_rate, global_step, dec_step, 
+        #     end_learning_rate=self.FLAGS.end_learning_rate, power=1.0)
+        # decay = self.FLAGS.decay
+        # learning_rate = tf.maximum(self.FLAGS.min_learning_rate, tf.train.exponential_decay(self.FLAGS.learning_rate, global_step, self.FLAGS.global_step, FLAGS.decay))
+
+
+        print('setting up optimizer')
+        self.optimizer = tf.train.AdamOptimizer(self.FLAGS.learning_rate)
+
+        print('computing gradients')
+        self.updates = self.optimizer.minimize(self.loss)
+        print('setting up saver')
+        self.saver = tf.train.Saver()
+
+    def setup_linear_system(self):
+        x = tf.reshape(self.images, shape=[-1, self.FLAGS.num_slices*self.FLAGS.image_height*self.FLAGS.image_width])
+        W = tf.get_variable('W', shape=[self.FLAGS.num_slices*self.FLAGS.image_height*self.FLAGS.image_width, 2],
+                            initializer=tf.contrib.layers.xavier_initializer())
+        b = tf.get_variable('b', shape=[2])
+        self.predictions = tf.matmul(x, W) + b
+
+    def setup_cnn_system(self):
+        """
+        model goes here
+        """
+        x = tf.reshape(self.images, [-1, self.FLAGS.num_slices, self.FLAGS.image_height, self.FLAGS.image_width, 1])
+
+        b1 = tf.layers.batch_normalization(x, training=self.is_training)
+
+        c1 = tf.layers.conv3d(b1, filters=16, kernel_size=[3, 3, 3], strides=[1,1,1], padding='same',
+                              kernel_initializer=tf.contrib.layers.xavier_initializer())
+
+        #r1 = tf.nn.relu(c1)
+        r1 = tf.maximum(c1, self.FLAGS.leak*c1)
+
+        m1 = tf.layers.max_pooling3d(r1, pool_size=2, strides=2, padding='valid')
+
+        shape = tf.shape(m1).as_list()
+
+        m1 = tf.reshape(m1, [shape[0], shape[1]*shape[2]*shape[3]*shape[4] ])
+        aff1_W = tf.get_variable('aff1_W', shape=[shape[1]*shape[2]*shape[3]*shape[4], 2048],
+                                 initializer=tf.contrib.layers.xavier_initializer())
+        aff1_b = tf.get_variable('aff1_b', shape=[2048])
+        a1 = tf.matmul(m1, aff1_W) + aff1_b
+        #a1 = tf.nn.relu(a1)
+        a1 = tf.maximum(a1, self.FLAGS.leak*a1)
+        aff2_W = tf.get_variable('aff2_W', shape=[2048, 2],
+                                 initializer=tf.contrib.layers.xavier_initializer())
+        aff2_b = tf.get_variable('aff2_b', shape=[2])
+
+        self.predictions = tf.matmul(a1, aff2_W) + aff2_b
+
+    def setup_loss(self):
+        """
+        Set up your loss computation here
+        :return:
+        """
+        with vs.variable_scope("loss"):
+            # self.start_answer (N)
+            self.loss = tf.losses.sparse_softmax_cross_entropy(self.labels, self.predictions)
+
+    def optimize(self, session, x_train, y_train):
+        """
+        Takes in actual data to optimize your model
+        This method is equivalent to a step() function
+        :return:
+        """
+        input_feed = {}
+
+        # fill in this feed_dictionary like:
+        # input_feed['train_x'] = train_x
+        input_feed[self.images] = x_train
+        input_feed[self.labels] = y_train
+        input_feed[self.is_training] = True
+
+        #print(x_train.shape)
+        # grad_norm, param_norm
+        output_feed = [self.updates, self.loss]
+
+
+        outputs = session.run(output_feed, input_feed)
+
+        return outputs
+
+    def test(self, session, x, y):
+        """
+        in here you should compute a cost for your validation set
+        and tune your hyperparameters according to the validation set performance
+        :return:
+        """
+        batch_indices = [0]
+        index = 0
+        while (True):
+            index += 1
+            if index == x.shape[0]:
+                if batch_indices[-1] != index:
+                    batch_indices.append(index)
+                break
+            if index % self.FLAGS.batch_size == 0:
+                batch_indices.append(index)
+        num_minibatches = len(batch_indices) - 1
+        losses = []
+        for b_end in range(1, num_minibatches + 1):
+            start = batch_indices[b_end-1]
+            end = batch_indices[b_end]
+            x_batch = x[start:end]
+            y_batch = y[start:end]
+
+            input_feed = {}
+
+            # fill in this feed_dictionary like:
+            # input_feed['valid_x'] = valid_x
+
+            input_feed[self.images] = x_batch
+            input_feed[self.labels] = y_batch
+            input_feed[self.is_training] = False
+
+            output_feed = [self.loss]
+
+            outputs = session.run(output_feed, input_feed)
+            losses += outputs # TODO: consider weighing last
+
+        return [sum(losses)/float(len(losses))]
+
+    def predict(self, session, x):
+        """
+        in here you should compute a cost for your validation set
+        and tune your hyperparameters according to the validation set performance
+        :return:
+        """
+        input_feed = {}
+
+        # fill in this feed_dictionary like:
+        # input_feed['valid_x'] = valid_x
+
+        input_feed[self.images] = x
+        input_feed[self.is_training] = False
+
+        output_feed = [self.predictions]
+
+        outputs = session.run(output_feed, input_feed)
+
+        return outputs
+
+    def accuracy(self, session, x, y):
+
+        batch_indices = [0]
+        index = 0
+        while (True):
+            index += 1
+            if index == x.shape[0]:
+                if batch_indices[-1] != index:
+                    batch_indices.append(index)
+                break
+            if index % self.FLAGS.batch_size == 0:
+                batch_indices.append(index)
+        num_minibatches = len(batch_indices) - 1
+        TP = 0
+        TN = 0
+        FP = 0
+        FN = 0
+        for b_end in range(1, num_minibatches + 1):
+            start = batch_indices[b_end-1]
+            end = batch_indices[b_end]
+            x_batch = x[start:end]
+            y_batch = y[start:end]
+
+            outputs = self.predict(session, x_batch)
+            probabilities = outputs[0]
+            pred = np.argmax(probabilities, axis=1)
+            # print(probabilities)
+            TP += np.sum(y_batch[y_batch == 1] == pred[y_batch == 1])
+            TN += np.sum(y_batch[y_batch == 0] == pred[y_batch == 0])
+            FP += np.sum(y_batch[y_batch == 0] != pred[y_batch == 0])
+            FN += np.sum(y_batch[y_batch == 1] != pred[y_batch == 1])
+
+        eps = 0.00001
+        acc = float(TP + TN) / (TP + TN + FP + FN + eps)
+        sens = float(TP) / float(TP + FN + eps)
+        spec = float(TN) / float(TN + FP + eps)
+        return acc, sens, spec
+
+    def train(self, session, dataset, train_dir):
+        logging.info("training:")
+        x_train, y_train, x_val, y_val = dataset
+
+        best_val_loss = self.FLAGS.best_val_loss
+        train_losses = []
+        val_losses = []
+        for e in range(self.FLAGS.epochs):
+            logging.info("="*80)
+            logging.info("Epoch %s of %s" % (e+1, self.FLAGS.epochs))
+            num_batches = int(math.ceil(x_train.shape[0]/self.FLAGS.batch_size))
+            epoch_train_losses = []
+            for i in range(num_batches):
+                batch_indices = random.sample(range(len(x_train)), self.FLAGS.batch_size)
+                x_train_batch = x_train[batch_indices]
+                y_train_batch = y_train[batch_indices]
+                _, train_loss = self.optimize(session, x_train_batch, y_train_batch)
+                epoch_train_losses.append(train_loss)
+                logging.info("batch %s/%s training loss: %s" % (i+1, num_batches, train_loss))
+            logging.info("-"*80)
+            train_losses += epoch_train_losses
+            logging.info("Validating epoch %s of %s" % (e+1, self.FLAGS.epochs))
+            train_accuracy, train_sens, train_spec = self.accuracy(session, x_train, y_train)
+            train_hm = (2*train_sens*train_spec) / (train_sens + train_spec)
+            logging.info("Training loss: %s" % (np.mean(np.asarray(epoch_train_losses))))
+            logging.info("Training: accuracy = %s, sensitivity = %s, specificity = %s" % (train_accuracy,
+                                                                                          train_sens, train_spec))
+
+            val_accuracy, val_sens, val_spec = self.accuracy(session, x_val, y_val)
+            val_loss = self.test(session, x_val, y_val)[0]
+            val_losses.append(val_loss)
+            logging.info("Validation loss: %s" % (val_loss))
+            logging.info("Validation: accuracy = %s, sensitivity = %s, specificity = %s" % (val_accuracy,
+                                                                                            val_sens, val_spec))
+            if val_loss < best_val_loss:
+                logging.info("NEW BEST VALIDATION LOSS: %s, SAVING!" % (val_loss))
+                best_val_loss = val_loss
+                self.saver.save(session, train_dir + 'model.weights')
+            logging.info("CURRENT BEST VALIDATION LOSS: %s" % (best_val_loss))
--- project-tf/train.py
+++ project-tf/train.py
@@ -4,7 +4,7 @@ import sys
 import tensorflow as tf
 
 import numpy as np
-from model import LungSystem
+from model2 import LungSystem
 from os.path import join as pjoin
 import random
 
@@ -14,7 +14,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 tf.app.flags.DEFINE_float("best_val_loss", float('inf'), "current best validation loss")
-tf.app.flags.DEFINE_string("model", 'cnn', "Type of model to use: linear or cnn")
+tf.app.flags.DEFINE_string("model", 'linear', "Type of model to use: linear or cnn")
 tf.app.flags.DEFINE_string("features", 'pixels', "Type of features to use: pixels or hog")
 tf.app.flags.DEFINE_integer("epochs", 10, "number of epochs")
 tf.app.flags.DEFINE_float("learning_rate", 0.001, "Learning rate.")
@@ -30,6 +30,7 @@ tf.app.flags.DEFINE_float("train_size", 0.6, "Size of train set")
 tf.app.flags.DEFINE_float("val_size", 0.2, "Size of val set")
 tf.app.flags.DEFINE_float("test_size", 0.2, "Size of test set")
 
+starting = True
 FLAGS = tf.app.flags.FLAGS
 train_dir = './weights/%s-%s/' % (FLAGS.model, FLAGS.features)
 
@@ -46,72 +47,76 @@ def initialize_model(session, model, train_dir):
     return model
 
 def main(_):
-    INPUT_FOLDER = './npy3d-data/'
-    patients = os.listdir(INPUT_FOLDER)
-    patient_images = {}
-    labels = {}
+	INPUT_FOLDER = './npy3d-data/'
+	patients = os.listdir(INPUT_FOLDER)
+	patient_images = {}
+	labels = {}
 
-    #storing images
-    for p in range(len(patients)):
-        image = np.load(INPUT_FOLDER + patients[p])
-        patient_images[patients[p][:-4]] = image
+	#storing images
+	for p in range(len(patients)):
+	    image = np.load(INPUT_FOLDER + patients[p])
+	    patient_images[patients[p][:-4]] = image
 
-    #storing labels
-    lines = [line for line in open('stage1_solution.csv')]
-    for i in range(1, len(lines)):
-        data = lines[i].split(',')
-        labels['1-'+data[0]] = int(data[1])
+	#storing labels
+	lines = [line for line in open('stage1_solution.csv')]
+	for i in range(1, len(lines)):
+	    data = lines[i].split(',')
+	    labels['1-'+data[0]] = int(data[1])
 
-    lines = [line for line in open('stage2_solution.csv')]
-    for i in range(1, len(lines)):
-        data = lines[i].split(',')
-        labels['2-'+data[0]] = int(data[1])
+	lines = [line for line in open('stage2_solution.csv')]
+	for i in range(1, len(lines)):
+	    data = lines[i].split(',')
+	    labels['2-'+data[0]] = int(data[1])
 
 
-    x = np.zeros((len(patient_images), FLAGS.num_slices, FLAGS.image_height, FLAGS.image_width), dtype=np.float32)
-    y = np.zeros(len(patient_images), dtype=np.int32)
-    
-    patient_keys = list(patient_images.keys())
-    for i in range(len(patient_images)):
-        patient = patient_keys[i]
-        x[i] = patient_images[patient]
-        y[i] = labels[patient]
-    num_total = x.shape[0]
-    num_test = int(np.round(num_total * FLAGS.test_size))
-    num_val = int(np.round(num_total * FLAGS.val_size))
-    num_train = int(np.round(num_total * FLAGS.train_size))
-    indices = list(range(num_total))
-    random.seed(8008135)
-    random.shuffle(indices)
-    test_indices = indices[:num_test]
-    val_indices = indices[num_test:num_test + num_val]
-    train_indices = indices[num_test + num_val:]
-    print('num_total: %s' % num_total)
-    print('num_test: %s' % num_test)
-    print('num_val: %s' % num_val)
-    print('num_train: %s' % num_train)
-    print('indices: %s' % indices)
-    print('test_indices: %s' % test_indices)
-    print('val_indices: %s' % val_indices)
-    print('train_indices: %s' % train_indices)
-    x_train = x[train_indices]
-    y_train = y[train_indices]
-    mean = np.mean(x_train, axis=0)
-    x_train -= mean
-    x_test = x[test_indices] - mean
-    y_test = y[test_indices]
-    x_val = x[val_indices] - mean
-    y_val = y[val_indices]
-    dataset = (x_train, y_train, x_val, y_val)
+	x = np.zeros((len(patient_images), FLAGS.num_slices, FLAGS.image_height, FLAGS.image_width), dtype=np.float32)
+	y = np.zeros(len(patient_images), dtype=np.int32)
 
-    lung_model = LungSystem(FLAGS)
+	patient_keys = list(patient_images.keys())
+	for i in range(len(patient_images)):
+	    patient = patient_keys[i]
+	    x[i] = patient_images[patient]
+	    y[i] = labels[patient]
+	num_total = x.shape[0]
+	num_test = int(np.round(num_total * FLAGS.test_size))
+	num_val = int(np.round(num_total * FLAGS.val_size))
+	num_train = int(np.round(num_total * FLAGS.train_size))
+	indices = list(range(num_total))
+	random.seed(8008135)
+	random.shuffle(indices)
+	test_indices = indices[:num_test]
+	val_indices = indices[num_test:num_test + num_val]
+	train_indices = indices[num_test + num_val:]
+	print('num_total: %s' % num_total)
+	print('num_test: %s' % num_test)
+	print('num_val: %s' % num_val)
+	print('num_train: %s' % num_train)
+	print('indices: %s' % indices)
+	print('test_indices: %s' % test_indices)
+	print('val_indices: %s' % val_indices)
+	print('train_indices: %s' % train_indices)
+	if starting:
+		x_train = x[train_indices][:32]
+		y_train = y[train_indices][:32]
+	else:
+	    x_train = x[train_indices]
+	    y_train = y[train_indices]
+	mean = np.mean(x_train, axis=0)
+	x_train -= mean
+	x_test = x[test_indices] - mean
+	y_test = y[test_indices]
+	x_val = x[val_indices] - mean
+	y_val = y[val_indices]
+	dataset = (x_train, y_train, x_val, y_val)
 
-    with tf.Session() as sess:
-        initialize_model(sess, lung_model, train_dir)
-        lung_model.train(sess, dataset, train_dir)
-        
+	lung_model = LungSystem(FLAGS)
 
-        # qa.evaluate_answer(sess, dataset, vocab, FLAGS.evaluate, log=True)
+	with tf.Session() as sess:
+	    initialize_model(sess, lung_model, train_dir)
+	    lung_model.train(sess, dataset, train_dir)
+	    
+
+	    # qa.evaluate_answer(sess, dataset, vocab, FLAGS.evaluate, log=True)
 
 if __name__ == "__main__":
     tf.app.run()
